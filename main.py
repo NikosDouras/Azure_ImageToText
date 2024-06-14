1104b798fb4b75248d910e21a4297e0e516dff0f
NikosDouras123 <Nikos.douras123@gmail.com>
2024-06-14 11:24:14 +0300

    Ready. 2 html pages working great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #main.py
-from flask import Flask, request, render_template, send_file
-import os, logging
+from flask import Flask, request, render_template, send_file, redirect, url_for
+import os
 from werkzeug.utils import secure_filename
 from PIL import Image
 from utilities import optimize_image, create_pdf, extract_text_from_image
@@ -8,8 +8,7 @@ from utilities import optimize_image, create_pdf, extract_text_from_image
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+
 
 @app.route('/')
 def index():
@@ -19,27 +18,24 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
-        logger.error("No file part in request")
         return "No file part"
 
     file = request.files['file']
     if file.filename == '':
-        logger.error("No selected file")
         return "No selected file"
+    file_content = file.read()
+    content_length = len(file_content)
 
     try:
-        file_content = file.read()
-        content_length = len(file_content)
-
         image = Image.open(file)
         width, height = image.size
     except IOError:
-        logger.error("Invalid image file")
         return "Invalid image file"
 
     file.seek(0)
 
-    if file and file.filename.lower().endswith('.jpg'):
+    # Allow both .jpg and .jpeg file types
+    if file and file.filename.lower().endswith(('.jpg', '.jpeg')):
         filename = secure_filename(file.filename)
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(filepath)
@@ -47,14 +43,13 @@ def upload_file():
         if (content_length > 4 * 1024 * 1024) or width <= 50 or height <= 50 or width >= 4200 or height >= 4200:
             optimized_filepath = os.path.join(app.config['UPLOAD_FOLDER'], f"optimized_{filename}")
             optimize_image(filepath, optimized_filepath)
-            return process_file(optimized_filepath)
+            pdf_path = process_file(optimized_filepath)
         else:
-            return process_file(filepath)
+            pdf_path = process_file(filepath)
 
-    logger.error("Invalid file type")
+        return redirect(url_for('thank_you', pdf_path=pdf_path))
+
     return "Invalid file type"
-
-
 
 
 
@@ -65,9 +60,18 @@ def process_file(filepath):
     pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], f"{os.path.splitext(os.path.basename(filepath))[0]}.pdf")
     create_pdf(text, pdf_path)
 
+    return pdf_path
+
+
+@app.route('/thankyou')
+def thank_you():
+    pdf_path = request.args.get('pdf_path', '')
+    return render_template('thankyou.html', pdf_path=pdf_path)
+
+
+@app.route('/download/<path:pdf_path>')
+def download_file(pdf_path):
     return send_file(pdf_path, as_attachment=True)
-
-
 
 
 if __name__ == '__main__':
